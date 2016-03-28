51c17abc3bdd8d6bfee33914ccb572ef7c0520a2
Shmulya <human.of.light@gmail.com>
2016-03-28 18:46:16 +0300

    Uploading files

---
+++ install.py
@@ -0,0 +1,116 @@
+import subprocess, os
+
+yes = set(['yes','y', 'ye', ''])
+no = set(['no','n'])
+fl = False
+flt = False
+
+def loop_input(msg): # Loop user unput. You can't input empty string
+    str = ""
+    while str.strip() == "":
+        str=raw_input(msg)
+    return str
+
+workdir = os.getcwd() 
+
+if os.path.exists(workdir+'/data') == True:
+
+    cho = raw_input('Use CA storage path by default [%s/ca]?: [y/n] '%workdir).lower()
+    if cho in yes:
+        certdir = workdir+'/ca'
+    elif cho in no:
+        certdir = loop_input('CA storage directory path: ')
+        if certdir[-1] == '/':
+            certdir = certdir[:-1]
+    
+    mail = loop_input('E-mail: ')
+    
+    while fl == False:
+        if os.path.exists(certdir)==False:
+            res = subprocess.call('mkdir "%s"'%certdir, shell=True)
+            res = subprocess.call('cd "%s" && rm -rf * && mkdir arc && touch serial && echo 01 > serial && touch index.txt'%(certdir), shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+            file = open('%s/data/templates/openssl.tmp'%workdir,'r').read()
+            conf = open('%s/openssl.cnf'%certdir,'w')
+            conf.write(file%(certdir,mail))
+            conf.close()
+            fl = True
+        else:
+            cho = raw_input('Directory %s already exist, do you want to use it (all files will be deleted)?: [y/n] ').lower()
+            if cho in yes:
+                res = subprocess.call('cd "%s" && rm -rf * && mkdir arc && touch serial && echo 01 > serial && touch index.txt'%(certdir), shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+                file = open('%s/data/templates/openssl.tmp'%workdir,'r').read()
+                conf = open('%s/openssl.cnf'%certdir,'w')
+                conf.write(file%(certdir,mail))
+                conf.close()
+                fl = True
+            if cho in no:
+                certdir = loop_input('Keystorage directory path: ')
+    if certdir != workdir+'/ca':
+        print '==============================='
+        print 'You set custom CA path, plese change CERTDIR variable in srv.py'
+        print '==============================='
+    c = loop_input('Coutry code(2 simbols): ')
+    o = loop_input('Organisation: ')
+    print '============='
+    print 'Change string "org" in srv.py'
+    print '============='
+    cn = loop_input('CA server name (Use your IP or domain): ')
+    print 'Attempt to create CA certificate and private key...'
+    res = subprocess.call('cd "%s" && openssl req -nodes -config "%s/openssl.cnf" -days 1825 -x509 -newkey rsa -keyout ca.key -out ca.crt -subj "/C=%s/O=%s/CN=%s"'%(certdir,certdir,c,o,cn), shell=True, stdout=open('./log/install.log', 'a'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'CA certificate and private key created'
+    elif res == 1:
+        print "Can't create CA certificate"
+    print 'Attempt to create DH2048, it can take a long time...'
+    res = subprocess.call('cd "%s" && openssl dhparam -out dh2048.pem 2048'%certdir,shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'DH created'
+    elif res == 1:
+        print "Can't create DH"
+    print 'Attempt to create TLS key...'
+    res = subprocess.call('cd "%s" && openvpn --genkey --secret ta.key'%certdir,shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'TLS key created'
+    elif res == 1:
+        print "Can't create TLS key"
+    print 'Attempt to create CRL...'
+    res = subprocess.call('cd "%s" && openssl ca -config "%s/openssl.cnf" -gencrl -out crl.pem'%(certdir,certdir),shell=True, stdout=open('./log/install.log', 'a'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'CRL created'
+    elif res == 1:
+        print "Can't create CRL"
+    print 'Attempt to create OpenVPN server key...'
+    res = subprocess.call('mkdir "%s/openvpn_key" && openssl genrsa -out "%s/openvpn_key/server.key"'%(certdir,certdir), shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'OpenVPN server key created'
+    elif res == 1:
+        print "Can't create OpenVPN server key"
+    res = subprocess.call('openssl req -new -nodes -config "%s/openssl.cnf" -key "%s/openvpn_key/server.key" -out "%s/openvpn_key/server.csr" -subj "/C=%s/O=%s/CN=%s"'%(certdir,certdir,certdir,c,o,cn),shell=True, stdout=open('./log/install.log', 'a'), stderr=subprocess.STDOUT)
+    print 'Attempt to sign OpenVPN server certificate...'
+    res = subprocess.call('openssl ca -batch -extensions server -config "%s/openssl.cnf" -in "%s/openvpn_key/server.csr" -notext -out "%s/openvpn_key/server.crt"'%(certdir,certdir,certdir),shell=True, stdout=open('./log/install.log', 'a'), stderr=subprocess.STDOUT)
+    if res == 0:
+        print 'OpenVPN certificate signed'
+        flt = True
+    elif res == 1:
+        print "Can't sign OpenVPN server certificate"
+    if flt == True:
+        print ''
+        print 'Your openvpn.conf below: '
+        print ''
+        open(workdir+'/start','w').write('cd "%s"\npython srv.py&'%workdir)
+        open(workdir+'/stop','w').write("PID=`ps aef | grep srv.py | grep -v grep | awk '{print ($1)}'`\nkill -9 $PID")
+        os.chmod(workdir+'/start', 0740)
+        os.chmod(workdir+'/stop', 0740)
+        prks = open(certdir+'/openvpn_key/server.key','r').read()
+        crts = open(certdir+'/openvpn_key/server.crt','r').read()
+        str = prks + '\n' + crts
+        itssl = open(certdir+'/sslcert.pem','w').write(str)
+        file = open('%s/data/templates/server.conf'%workdir,'r').read()
+        print file%(certdir,certdir,certdir,certdir,certdir,certdir)
+    else:
+        print ''
+        print 'Error, read %s/log/ for more information'%workdir
+else:
+    print '=============================='
+    print 'Can\'t find data directory. Check that install.py started from directory where it placed'
+    print '=============================='
---
+++ srv.py
@@ -0,0 +1,188 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl, subprocess, json, re, os, logging
+#from twisted.test.test_hook import subPost
+from urllib import unquote
+
+
+WORKDIR = os.getcwd()
+CERTDIR = WORKDIR+'/ca' # DO NOT CHANGE IF DEFAULT INSTALL path to your certificate storage (keystorage from install)
+ipadr = '127.0.0.1' #IP for HTTPS connection
+org = 'OpenVPN_Server' #Your company
+certFile = CERTDIR+'/sslcert.pem'
+logging.basicConfig(format='[%(levelname)s] [%(asctime)s] - %(message)s', filename=WORKDIR+'/log/crt.log', level=logging.WARN)
+
+
+def userconfig(cn):
+    tmpl = open('%s/data/templates/client.conf'%WORKDIR,'r').read()
+    conf = open('%s/%s.conf'%(CERTDIR,cn),'w')
+    conf.write(tmpl%(ipadr,cn,cn,cn,cn,cn,cn))
+    conf.close()
+
+def certlist():
+    crtbase = open(CERTDIR+'/index.txt','r')
+    crtinfolist = []
+    for line in crtbase:
+        crtinfolist.append(line.split())
+    n = 0
+    for el in crtinfolist:
+        if crtinfolist[n][0] == 'V':
+            crtinfolist[n][4] = el[4][1:].split('/')
+            flnm = crtinfolist[n][4][2].split('=')[1]
+            if os.path.exists('%s/arc/%s.tar'%(CERTDIR,flnm)) == True:
+                 crtinfolist[n].append('1')
+            else:
+                crtinfolist[n].append('0')
+            n = n + 1
+        elif crtinfolist[n][0] == 'R':
+            crtinfolist[n][5] = el[5][1:].split('/')
+            crtinfolist[n].append('0')
+            n = n + 1
+    return crtinfolist
+
+def response(cl,cont,resp=200,type='text/html'):
+    cl.send_response(resp)
+    cl.send_header("Content-type", type)
+    cl.end_headers()
+    cl.wfile.write(cont)
+
+def certgen(cn,f,typ,c='RU',o=org, csr=None):
+    if f == '0':
+        res = subprocess.call('openssl genrsa -out "%s/%s.key"'%(CERTDIR,cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+        if res == 0:
+            logging.debug('Private key for %s created'%cn)
+        elif res == 1:
+            logging.error("Can't create private key for "+cn)
+        res = subprocess.call('openssl req -new -nodes -config "%s/openssl.cnf" -key "%s/%s.key" -out "%s/%s.csr" -subj "/C=%s/O=%s/CN=%s"'%(CERTDIR,CERTDIR,cn,CERTDIR,cn,c,o,cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+        userconfig(cn)
+        if typ == 'srv':
+            ext = '-extensions server'
+        elif typ == 'clnt':
+            ext = '-extensions usr_cert'
+        res = subprocess.call('openssl ca -batch %s -config "%s/openssl.cnf" -in "%s/%s.csr" -notext -out "%s/%s.crt"'%(ext,CERTDIR,CERTDIR,cn,CERTDIR,cn),shell=True, stdout=open('./log/crt.log', 'a'), stderr=subprocess.STDOUT)
+        if res == 0:
+            logging.debug('Certificate for %s signed'%cn)
+            fl = True
+            res = subprocess.call('rm -rf "%s/%s.csr"'%(CERTDIR,cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+            res = subprocess.call('cd "%s" && tar -cvf arc/%s.tar %s.* ca.crt ta.key'%(CERTDIR,cn,cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+            res = subprocess.call('rm -rf %s/%s.*'%(CERTDIR.replace(' ', '\ '),cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+        elif res == 1:
+            fl = False
+            logging.error("Can't sign certificate for "+cn)
+    if f == '1':
+        file = open('./tmp/%s.csr'%cn,'w')
+        print unquote(csr)
+        n = 0
+        str = ''
+        for lin in unquote(csr).split('\n'):
+            n = n + 1
+            if n == 1 or n == len(unquote(csr).split('\n')):
+               lin = lin.replace('+',' ')
+            str = str + lin + '\n'
+        file.write(str)
+        file.close()
+        if typ == 'srv':
+            ext = '-extensions server'
+        elif typ == 'clnt':
+            ext = '-extensions usr_cert'
+        res = subprocess.call('openssl ca -batch %s -config "%s/openssl.cnf" -in "%s/tmp/%s.csr" -out "%s/%s.crt"'%(ext,CERTDIR,cn,WORKDIR,CERTDIR,cn),shell=True, stdout=open('./log/crt.log', 'a'), stderr=subprocess.STDOUT)
+        if res == 1:
+            fl = False
+            logging.error("Can't sign certificate for "+cn)
+        else:
+            fl = True
+            logging.debug('Certificate for %s signed'%cn)
+            res = subprocess.call('cd "%s" && tar -cvf arc/%s.tar %s.* ca.crt ta.key'%(CERTDIR,cn,cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+            res = subprocess.call('rm -rf %s/tmp/%s.csr'%(WORKDIR.replace(' ', '\ '),cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    return fl
+            
+def revoke(sn,cn):
+    res = subprocess.call('openssl ca -config "%s/openssl.cnf" -revoke "%s/%s.pem"'%(CERTDIR,CERTDIR,sn), shell=True, stdout=open('./log/crt.log', 'a'), stderr=subprocess.STDOUT)
+    if res == 1:
+        fl = False
+        logging.error("Can't revoke certificate for "+cn)
+    else:
+        fl = True
+        logging.debug('Certificate for %s revoked'%cn)
+    res = subprocess.call('openssl ca -config "%s/openssl.cnf" -gencrl -out "%s/crl.pem"'%(CERTDIR,CERTDIR), shell=True, stdout=open('./log/crt.log', 'a'), stderr=subprocess.STDOUT)
+    if res == 1:
+        fl = False
+    else:
+        fl = True
+    res = subprocess.call('rm -rf %s/arc/%s.tar'%(CERTDIR.replace(' ', '\ '),cn),shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    return fl
+
+class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            response(self,open(WORKDIR+'/data/pages/index.html','r').read())
+        elif self.path == '/crl.pem':
+            response(self, open(CERTDIR+'/crl.pem','rb').read(), type='application/x-pem-file')
+        srch = re.search('arc/', self.path)
+        if srch != None:
+            response(self, open(CERTDIR+self.path,'rb').read(), type='application/x-tar')
+        srch1 = re.search('.js$', self.path)
+        if srch1 != None:
+            response(self, open(WORKDIR+'/data/pages/'+self.path,'rb').read(), type='text/javascript')
+        srch2 = re.search('images', self.path)
+        if srch2 != None:
+            response(self, open(WORKDIR+'/data/pages/'+self.path,'rb').read(), type='image/jpeg')
+        srch3 = re.search('.css$', self.path)
+        if srch3 != None:
+            response(self, open(WORKDIR+'/data/pages/'+self.path,'rb').read(), type='text/css')        
+    def do_POST(self):
+        if self.path == '/api':
+            content_len = int(self.headers.getheader('content-length', 0))
+            post_body = self.rfile.read(content_len)
+            post_body = post_body.split('&')
+            req = []
+            for it in post_body:
+                f = it.split('=')
+                req.append(f[1])
+            try:
+                if req[0] == 'crtget':
+                    response(self, json.dumps(certlist()), type='application/json')
+                elif req[0] == 'crtgen':
+                    if req[2] == '0': 
+                        res = certgen(req[1],req[2],req[3])
+                        if res == True:
+                            logging.debug('New certificate for CN=%s created'%req[1])
+                            response(self,'Done')
+                        else:
+                            logging.error('Can\'t create new certificate and key, CN is '+req[1])
+                            response(self,'Error, see server log', 500)
+                    elif req[2] == '1':
+                        res = certgen(req[1],req[2],req[3],csr=req[4])
+                        if res == True:
+                            logging.debug('Certificate from csr for CN=%s created'%req[1])
+                            response(self,'Done')
+                        else:
+                            logging.error('Can\'t sign certificate from csr, CN is '+req[1])
+                            response(self,'Error, see server log', 500)
+                elif req[0] == 'crtrev':
+                    res = revoke(req[1],req[2])
+                    if res == True:
+                        logging.debug('Certificate for CN=%s revoked'%req[2])
+                        response(self, 'Done')
+                    else:
+                        logging.error('Can\'t revoke certificate, CN is '+req[2])
+                        response(self,'Error, see server log', 500)
+            except Exception:
+                logging.error('Check arguments in post request from client, post body is %s'%post_body)
+                response(self, 'Error, see server log', 500)
+            except IndexError:
+                print 'Err'
+    def log_message(self, format, *args):
+        logstr = '[%s] - %s %s\n'%(self.log_date_time_string(),self.address_string(),format%args)
+        logging.debug(logstr)
+
+if os.path.exists(WORKDIR+'/data') == True:
+    
+    httpd = BaseHTTPServer.HTTPServer((ipadr, 4443), MyHandler)
+    
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile=certFile, server_side=True)
+    httpd.serve_forever()
+else:
+    print '==================================================================================='
+    print 'Can\'t find data directory. Check that srv.py started from directory where it placed'
+    print '==================================================================================='
